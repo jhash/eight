c2065fd438cec511f85e819ea02bf7673bcf9c3b
Jake Hash <1508098+jhash@users.noreply.github.com>
2025-07-25 17:09:29 -0400

    Add superadmin role and task to promote users (#7)
    * Add superadmin role and task to promote users
    
    * Update brakeman

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,6 +1,6 @@
 class Identity < ApplicationRecord
   belongs_to :user
-  
+
   validates :provider, presence: true
   validates :uid, presence: true
   validates :uid, uniqueness: { scope: :provider }
